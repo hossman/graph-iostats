b1f9716e58324c48dc032f0ae322a10fdfd93cf3
Chris Hostetter <hossman_github@fucit.org>
2025-01-02 15:09:11 -0700

    Refactor device grepping to tmp filed out of every individual gnuplot script and into disk-report.sh

--- disk-report.sh
+++ disk-report.sh
@@ -14,9 +14,14 @@ fi
 DISK=$1
 IOSTATS=$2
 
-$CWD/iostat-readwrite-requests.sh $IOSTATS $DISK $DISK-requests.png
-$CWD/iostat-readwrite-throughput.sh $IOSTATS $DISK $DISK-throughput.png
-$CWD/iostat-queue-length.sh $IOSTATS $DISK $DISK-queue-length.png
-$CWD/iostat-queue-size.sh $IOSTATS $DISK $DISK-queue-size.png
-$CWD/iostat-service.sh $IOSTATS $DISK $DISK-service.png
-$CWD/iostat-utilisation.sh $IOSTATS $DISK $DISK-utilisation.png
+DISK_IOSTATS=$(mktemp "$IOSTATS.$DISK.tmp.XXXXXX")
+cat $IOSTATS | grep "$DISK " > $DISK_IOSTATS
+
+$CWD/iostat-readwrite-requests.sh $DISK_IOSTATS $DISK $DISK-requests.png
+$CWD/iostat-readwrite-throughput.sh $DISK_IOSTATS $DISK $DISK-throughput.png
+$CWD/iostat-queue-length.sh $DISK_IOSTATS $DISK $DISK-queue-length.png
+$CWD/iostat-queue-size.sh $DISK_IOSTATS $DISK $DISK-queue-size.png
+$CWD/iostat-service.sh $DISK_IOSTATS $DISK $DISK-service.png
+$CWD/iostat-utilisation.sh $DISK_IOSTATS $DISK $DISK-utilisation.png
+
+rm $DISK_IOSTATS
